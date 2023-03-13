179854fca80ce3077561f4e24b294497067a0350
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2023-03-13 17:41:42 +0000

    feat: adding home page

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div class="frame">
+      <div class="card">
+        <div class="flip hvr-outline-in">
+          <div class="front">
+            <h1>hey.</h1>
+            <p class="flip-me">Flip Me</p>
+            <div class="line line-1"></div>
+            <div class="line line-2"></div>
+            <i class="fa fa-heart"></i>
+          </div>
+
+          <div class="back">
+            <i class="fa fa-heart"></i>
+            <h3>Krerkkiat</h3>
+            <div class="line line-3"></div>
+            <h2>Developer</h2>
+            <div class="social">
+              <i class="fab fa-codepen"></i>
+              <i class="fab fa-twitter"></i>
+              <i class="fab fa-linkedin-in"></i>
+              <i class="fab fa-github"></i>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
