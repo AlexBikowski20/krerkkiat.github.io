c1449f5a79d9cb094be994b375203dbce010a72f
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2023-03-13 18:02:45 +0000

    feat: add routing and contract page

---
+++ frontend/app/src/ContactPage.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function ContactPage() {
+
+  return (
+    <>
+    <p>Contact page</p>
+    <Link to={`/`}>Home apge</Link>
+    </>
+  );
+}
+
+export default ContactPage;
--- frontend/app/src/main.jsx
+++ frontend/app/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+
+import ContactPage from './ContactPage'
+import App from './App'
+
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
